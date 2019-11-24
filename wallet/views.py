35f440d97abbf28f181eb0bbd7b0fe06e7c8f3ca
Louieee <louis.paul9095@gmail.com>
2019-11-24 03:23:08 +0100

    CORRECTION 7

--- wallet/views.py
+++ wallet/views.py
@@ -359,7 +359,9 @@ def activate(request, uidb64, token):
         else:
             if user.is_active is False:
                 user.delete()
-            return render(request, 'wallet/home.html', {'message': 'Your Email was invalid, Therefore Your Account Has '
+                return render(request, 'wallet/home.html', {'message': 'Your Email was invalid, Therefore Your Account Has '
                                                                    'been deleted', 'status': 'danger'})
+            else:
+                return redirect('/')
     except Investor.DoesNotExist:
         return render(request, 'wallet/home.html', {'message': 'User Does Not Exist', 'status': 'danger'})
