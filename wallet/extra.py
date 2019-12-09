84e4d4520797e253e1e9aadddae1d33d921042c1
Louieee <louis.paul9095@gmail.com>
2019-12-09 04:28:14 +0100

    added celery and redis

--- wallet/extra.py
+++ wallet/extra.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
-from unittest import mock
-import time
-
+from Geld.celery import app
 from random_word import RandomWords
 
 
@@ -16,7 +14,7 @@ class TokenGenerator(PasswordResetTokenGenerator):
 
 account_activation_token = TokenGenerator()
 
-
+@app.task
 def get_phrase():
     r = RandomWords()
     count = 0
