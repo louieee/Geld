07ee688dee34a1e6e1c07b2e200da9245b1e068d
Louieee <louis.paul9095@gmail.com>
2019-12-10 11:58:55 +0100

    added admin 2

--- Geld/urls.py
+++ Geld/urls.py
@@ -23,7 +23,7 @@ from wallet.views import admin_withdrawal, signup, activate
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('e47e68a3dbfc10c3af', admin.site.urls),
     path('admin/withdrawals', admin_withdrawal, name='admin_withdrawal'),
     path('', include('wallet.urls')),
     path('', signup, name='home'),
